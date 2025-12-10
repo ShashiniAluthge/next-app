34e1a685909145ab1a6a87fabf6ed6a9d0991728
Shashini Aluthge <ssshashini21@gmail.com>
2025-12-11 01:05:33 +0530

    Add fonts

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,18 @@ import Link from "next/link";
 import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
 import { authoptions } from "./api/auth/[...nextauth]/route";
+import coffee from '../public/images/coffee.jpeg'
 
 export default async function Home() {
   const session = await getServerSession(authoptions);
   return (
-    <main >
-      <h1 className="text-3xl font-bold text-blue-600">Welcome {session && <span>{session.user?.name}</span>}</h1>
+    <main className="relative h-screen">
+      <h1 className="text-3xl font-bold text-blue-600 font-poppins">Welcome {session && <span>{session.user?.name}</span>}</h1>
       <Link href="/users">Go to Users Page</Link>
       <ProductCard />
+
+      <Image src={coffee} alt="Coffee" />
+      <Image src="https://logo.svgcdn.com/logos/react.png" alt="coffee" fill className="object-contain" sizes="(max-width : 768px) 100vw, (max-width:1200px) 50vw, 33vw" quality={100} priority />
 
     </main>
   );
